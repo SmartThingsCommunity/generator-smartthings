368f9d672e4c9db83e4dbb19cd9515d045e61834
Eric Rodewald <eric.rode@gmail.com>
2019-08-01 10:06:26 -0500

    feat: add ci publishing (#12)
    Include required packages
    apply xo formatting
    Update root gitignore
    add codecov and release configs
    update circleci config
    Fix validator
    Add more code coverage

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,26 +5,26 @@ const yosay = require('yosay')
 const Generator = require('yeoman-generator')
 
 module.exports = class extends Generator {
-  async prompting() {
-    this.log(yosay(`Welcome to the ${chalk.hex('#15bfff').bold.underline('SmartThings')} generator!`))
+	async prompting() {
+		this.log(yosay(`Welcome to the ${chalk.hex('#15bfff').bold.underline('SmartThings')} generator!`))
 
-    this.answers = await this.prompt([{
-      type: 'list',
-      name: 'language',
-      message: chalk.hex('#15bfff').bold.underline('What language do you want to use?'),
-      choices: [{
-        name: 'NodeJS',
-        value: 'node'
-      }, {
-        name: 'Java',
-        value: 'java'
-      }]
-    }])
-  }
+		this.answers = await this.prompt([{
+			type: 'list',
+			name: 'language',
+			message: chalk.hex('#15bfff').bold.underline('What language do you want to use?'),
+			choices: [{
+				name: 'NodeJS',
+				value: 'node'
+			}, {
+				name: 'Java',
+				value: 'java'
+			}]
+		}])
+	}
 
-  async end() {
-    this.env.run(`smartthings:${this.answers.language}`, {
-      skipWelcome: true
-    })
-  }
+	async end() {
+		this.env.run(`smartthings:${this.answers.language}`, {
+			skipWelcome: true
+		})
+	}
 }
